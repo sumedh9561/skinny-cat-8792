9f186e6c37ea2067e112e6bbe84db7a87be8dfe2
Abideshverma <samabidesh143@gmail.com>
2022-11-11 12:33:52 +0530

    bean class

--- skinny-cat-8792/src/main/java/com/masai/model/Product.java
+++ skinny-cat-8792/src/main/java/com/masai/model/Product.java
@@ -1,5 +1,50 @@
 package com.masai.model;
 
-public class Product {
+import javax.persistence.Embedded;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Size;
 
+
+
+public class Product {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Integer productId;
+	
+	@Size(min = 3, max = 25, message = "Product Name should contain min 3 character." )
+	private String productName;
+	
+	@Min(value = 1, message = "Please Enter a valid price")
+	private Double price;
+	
+	
+	private String color;
+	
+	private String dimension;
+	
+	private String manufacturer;
+	
+	@Min(value = 1, message = "Please Enter a valid quantity.")
+	private Integer quantity;
+	
+	@Embedded
+	private Category category;
+
+	public Product(
+			@Size(min = 3, max = 25, message = "Product Name should contain min 3 character.") String productName,
+			@Min(value = 1, message = "Please Enter a valid price") Double price, String color, String dimension,
+			String manufacturer, @Min(value = 1, message = "Please Enter a valid quantity.") Integer quantity,
+			Category category) {
+		super();
+		this.productName = productName;
+		this.price = price;
+		this.color = color;
+		this.dimension = dimension;
+		this.manufacturer = manufacturer;
+		this.quantity = quantity;
+		this.category = category;
+	}
 }
